58e0ce9676945da446c12952c3d3a49e956d6da1
huilv <1204483693@qq.com>
2016-02-19 22:38:08 +0800

    1111111111111111

---
+++ MyApplication/app/src/main/java/cc/bb/aa/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package cc.bb.aa.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
